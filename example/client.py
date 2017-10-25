4d3d6d182084522f2c1c687aff00e00e312aa76f
Kevin Jing Qiu <kevin@idempotent.ca>
2017-10-25 14:07:09 -0400

    Code formatting

--- example/client.py
+++ example/client.py
@@ -11,56 +11,60 @@ ls_tracer = lightstep.tracer.init_tracer(group_name="example client", access_tok
 # this tracer does not trace all requests, so the @tracer.trace() decorator must be used
 tracer = FlaskTracer(ls_tracer)
 
+
 @app.route("/")
 def index():
-	'''
-	Index page, has no tracing.
-	'''
-	return "Index Page"
+    '''
+    Index page, has no tracing.
+    '''
+    return "Index Page"
 
 
 @app.route("/request/<script>/<int:numrequests>")
 @tracer.trace("url")
 def send_multiple_requests(script, numrequests):
-	'''
-	Traced function that makes a request to the server
-	Injects the current span into headers to continue trace
-	'''
-	span = tracer.get_span()
-	def send_request():
-		url = "http://localhost:5000/"+str(script)
-		request = urllib2.Request(url)
-		inject_as_headers(ls_tracer, span, request)
-		try:
-			response = urllib2.urlopen(request)
-		except urllib2.URLError as ue:  
-			response = ue
-	for i in range(numrequests):
-		send_request()
-	return "Requests sent"
+    '''
+    Traced function that makes a request to the server
+    Injects the current span into headers to continue trace
+    '''
+    span = tracer.get_span()
+
+    def send_request():
+        url = "http://localhost:5000/"+str(script)
+        request = urllib2.Request(url)
+        inject_as_headers(ls_tracer, span, request)
+        try:
+            response = urllib2.urlopen(request)
+        except urllib2.URLError as ue:
+            response = ue
+    for i in range(numrequests):
+        send_request()
+    return "Requests sent"
+
 
 @app.route('/log')
 @tracer.trace()
 def log_something():
-	'''
-	Traced function that logs something to the current 
-	request span.
-	'''
-	span = tracer.get_span()
-	span.log_event("hello world")
-	return "Something was logged"
+    '''
+    Traced function that logs something to the current
+    request span.
+    '''
+    span = tracer.get_span()
+    span.log_event("hello world")
+    return "Something was logged"
+
 
 @app.route("/test")
 @tracer.trace()
 def test_lightstep_tracer():
-	'''
-	Simple traced function to ensure the tracer works.
-	'''
-	return "No errors"
+    '''
+    Simple traced function to ensure the tracer works.
+    '''
+    return "No errors"
+
 
 def inject_as_headers(tracer, span, request):
     text_carrier = {}
     tracer.inject(span.context, opentracing.Format.TEXT_MAP, text_carrier)
     for k, v in text_carrier.iteritems():
-        request.add_header(k,v)
-
+        request.add_header(k, v)
--- example/server.py
+++ example/server.py
@@ -6,36 +6,36 @@ import urllib2
 
 app = Flask(__name__)
 
+
 # one-time tracer initialization code
 ls_tracer = lightstep.tracer.init_tracer(group_name="example server", access_token="{your_lightstep_token}")
 # This tracer traces all requests
 tracer = FlaskTracer(ls_tracer, True, app, ["url_rule"])
 
+
 @app.route("/simple")
 def simple_response():
-	'''
-	This request will be automatically traced.
-	'''
-	return "Hello, world!"
+    '''
+    This request will be automatically traced.
+    '''
+    return "Hello, world!"
+
 
 @app.route("/childspan")
 def create_child_span():
-	'''
-	This request will also be automatically traced.
-	
-	This is a more complicated example of accessing the current 
-	request from within a handler and creating new spans manually.
-	'''
-	parent_span = tracer.get_span()
-	child_span = ls_tracer.start_span("inside create_child_span", child_of=parent_span)
-	ans = calculate_some_stuff()
-	child_span.finish()	
-	return str(ans)
+    '''
+    This request will also be automatically traced.
+
+    This is a more complicated example of accessing the current
+    request from within a handler and creating new spans manually.
+    '''
+    parent_span = tracer.get_span()
+    child_span = opentracing.tracer.start_span("inside create_child_span", child_of=parent_span)
+    ans = calculate_some_stuff()
+    child_span.finish()
+    return str(ans)
+
 
 def calculate_some_stuff():
-	two = 1 + 1
-	return two
-
-
-
-
+    two = 1 + 1
+    return two
